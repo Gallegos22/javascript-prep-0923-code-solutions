4f0ccd559fefe3e500a7a07680a958705a12664f
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2023-10-05 18:20:34 +0000

    Completed Prep-Javascript-objects Exercise

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,21 @@
+const person = {
+  firstName: 'Eduardo',
+  lastName: 'Gallegos',
+  job: 'Electrician',
+  previousJob: 'Door Dasher',
+  hobby: 'Fishing',
+};
+console.log(person);
+
+const fullName =
+  " The person's name is: " + person.firstName + ' ' + person.lastName + '.';
+console.log(fullName);
+
+const currentJob = " The person's current job is: " + person.job + '.';
+console.log(currentJob);
+
+const previousJob =
+  " The person's previous job is: " + person['previousJob'] + '.';
+console.log(previousJob);
+
+console.log(person);
