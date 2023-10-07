8ae7ef8547b7eafa99fc77711836c6c00d05f253
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2023-10-07 19:34:44 +0000

    Updated Prep-Javascript-Arrays Exercise

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -1,14 +1,17 @@
 const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
-console.log(languages);
+console.log('languages', languages);
 
 const firstElement = languages[0];
-console.log(firstElement);
+console.log('the first element of the array is', firstElement);
 
 const thirdElement = languages[2];
-console.log(thirdElement);
+console.log('the third element of the array is', thirdElement);
 
 const length = languages.length;
-console.log(length);
+console.log('the length of the array is', length);
 
-const lastElement = languages[5];
-console.log(lastElement);
+const lastIndex = length - 1;
+
+const lastElement = languages[lastIndex];
+
+console.log('the last element of the array is', lastElement);
